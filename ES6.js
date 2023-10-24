6dd4b6b855020f41e60adb94758b86787b70af59
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-24 17:18:13 +0100

    write arrow functions with default parameters

--- ES6.js
+++ ES6.js
@@ -47,3 +47,7 @@ const magic =() => new Date();
 // we can write arrow functions with parameters
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// we can write arrow functions with default parameters
+const increment = (number, value = 1) => number + value;
+console.log(increment(5, 2));

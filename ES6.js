8d5d9e4691d15f0d0543dd5beed2dc784b1256a7
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-24 13:34:29 +0100

    Mutate an Array Declared with const

--- ES6.js
+++ ES6.js
@@ -1,6 +1,4 @@
 // Compare Scopes of the var and let Keywords
-
-
 function checkScope (){
     "use strict";
     let i = "function scope";
@@ -15,3 +13,13 @@ checkScope();
 
 
 // Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace(){
+    "use strict";
+    // s = [2, 5, 7]; <- this is invalid
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+editInPlace();
+console.log(s);

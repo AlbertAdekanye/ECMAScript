e3e862a40196e69df42ada94800c140552ac6ed4
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-24 17:06:00 +0100

    // we can write arrow functions with parameters

--- ES6.js
+++ ES6.js
@@ -43,3 +43,7 @@ console.log(PI);
 
 // we use arrow functions to write anonymous functions
 const magic =() => new Date();
+
+// we can write arrow functions with parameters
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));

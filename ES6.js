a5a8d95e0719f1df477ad4e8134e2eb4e610c3e9
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-25 14:49:42 +0100

    #

--- ES6.js
+++ ES6.js
@@ -118,3 +118,23 @@ const stats = {
 }
 const half = ({max, min}) => (max + min) / 2.0;
 console.log(half(stats));
+
+// create strings using template literals
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"],
+}
+function makeList(arr){
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++){
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    return failureItems;
+}
+const failuresList = makeList(result.failure);
+console.log(failuresList);
+
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+const createPerson = (name, age, gender) => ({name, age, gender});
+console.log(createPerson())

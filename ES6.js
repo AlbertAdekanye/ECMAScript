af3e7d18bedbccc9d3400b42ac0739351c2c450c
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-25 12:57:33 +0100

    Use Destructuring Assignment to Extract Values from Objects

--- ES6.js
+++ ES6.js
@@ -67,3 +67,12 @@ const arr1 = ['jan', 'feb', 'mar', 'apr', 'may'];
 let arr2;
 arr2 = [...arr1];
 console.log(arr2);
+
+// Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80,
+}
+const {today, tomorrow, yesterday} = HIGH_TEMPERATURES;
+console.log(today, tomorrow, yesterday);
